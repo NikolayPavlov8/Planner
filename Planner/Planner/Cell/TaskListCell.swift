5bc9f6801deb3b614271cac2eae1f4182ceadcca
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-06-07 13:32:31 +0300

    code16

---
+++ Planner/Planner/Cell/TaskListCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskListCell.swift
+//  Planner
+//
+//  Created by Nikolai Pavlov on 07/06/2019.
+//  Copyright © 2019 Nikolai Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class TaskListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
